d170fe15bf418a0d5e5a8e1f5db52ad67bbdd506
ejsadiarin <ejsadiarin@gmail.com>
2024-01-02 04:40:51 +0800

    transparent background enable

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -9,7 +9,7 @@ return {
       vim.g.gruvbox_material_disable_italic_comment = 1
       vim.g.gruvbox_material_enable_italic = 0
       vim.g.gruvbox_material_enable_bold = 1
-      vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_transparent_background = 1
       -- Themes
       vim.g.gruvbox_material_foreground = "mix"
       vim.g.gruvbox_material_background = "hard"
